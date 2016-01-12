b050cdb83f9dbac50240a8ff45fbda2f6bb951f6
Thibault Lenclos <tlenclos@jolicode.com>
2016-01-12 10:15:55 +0100

    Demo for desktop, tablet and mobile layout

--- demo/index.js
+++ demo/index.js
@@ -4,9 +4,24 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var ReactSwipe = require('../react-swipe');
 
-var Panes = Array.apply(null, Array(20)).map(function (_, i) {
+var PanesDesktop = Array.apply(null, Array(20)).map(function (_, i) {
   return React.createElement('div', {key: i},
-      React.createElement('b', null, i)
+    React.createElement('b', {className: 'desktop'}, i),
+    React.createElement('b', {className: 'desktop'}, i),
+    React.createElement('b', {className: 'desktop'}, i)
+  );
+});
+
+var PanesTablet = Array.apply(null, Array(20)).map(function (_, i) {
+  return React.createElement('div', {key: i},
+      React.createElement('b', {className: 'tablet'}, i),
+      React.createElement('b', {className: 'tablet'}, i)
+  );
+});
+
+var PanesMobile = Array.apply(null, Array(20)).map(function (_, i) {
+  return React.createElement('div', {key: i},
+    React.createElement('b', {className: 'mobile'}, i)
   );
 });
 
@@ -23,15 +38,22 @@ var Page = React.createClass({
     return (
       React.createElement('div', null,
         React.createElement('h1', null, 'ReactSwipe'),
-        React.createElement('h2', null, 'Open this page from a mobile device (real or emulated).'),
+        React.createElement('h2', null, 'Mobile'),
         React.createElement(ReactSwipe, {
           ref: 'ReactSwipe',
           id: 'mySwipe'
-        }, Panes),
-        React.createElement('div', {style: {textAlign: 'center'}},
-          React.createElement('button', {onClick: this.prev}, 'Prev'),
-          React.createElement('button', {onClick: this.next}, 'Next')
-        )
+        }, PanesMobile),
+        React.createElement('h2', null, 'Tablet'),
+        React.createElement(ReactSwipe, {
+          ref: 'ReactSwipe',
+          id: 'mySwipe'
+        }, PanesTablet),
+        React.createElement('h2', null, 'Desktop'),
+        React.createElement(ReactSwipe, {
+          ref: 'ReactSwipe',
+          id: 'mySwipe'
+        }, PanesDesktop),
+        React.createElement('div', {style: {textAlign: 'center'}})
       )
     );
   }
